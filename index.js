e71182b5a0d507bf89bb8729dfb4106a2d7149d1
Akash Kumar Singh <akash.singh22396@gmail.com>
2020-12-17 11:56:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 const filterArray = (arrayData, searchValue, objectName) => {
-  console.log(objectName)
   let filterValue = arrayData.filter((data) => {
     return objectName ? data[objectName] === searchValue : data === searchValue;
   });
@@ -8,15 +7,15 @@ const filterArray = (arrayData, searchValue, objectName) => {
 };
 
 // console.log(filterArray([1, 2, 3, 4, 5], 1));
-console.log(
-  filterArray(
-    [
-      { name: "akash", age: 25 },
-      { name: "ravi", age: 28, liveIn: { city: "delhi" } },
-      { name: "rajan", age: 25 },
-    ],
-    28,
-    "age"
-  )
-);
+// console.log(
+//   filterArray(
+//     [
+//       { name: "akash", age: 25 },
+//       { name: "ravi", age: 28, liveIn: { city: "delhi" } },
+//       { name: "rajan", age: 25 },
+//     ],
+//     28,
+//     "age"
+//   )
+// );
 module.exports = filterArray;
